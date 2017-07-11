50346b6f8174d561c8e11f4d600c56594491cab5
talbot <talbotmd@stanford.edu>
2017-07-11 15:11:14 -0700

    removed non working scripts

--- run.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-
-subprocess.call(['"./Documents/human_force_code/human_force_code/optitrack/bin/demo_project"'])
--- run.sh
+++
@@ -1,8 +0,0 @@
-sudo ./optoforcesensors/sensor1/build/optoforce_driver &
-
-sleep 5
-
-sudo ./optoforcesensors/sensor2/build/optoforce_driver &
-
-
-./optitrack/bin/demo_project
