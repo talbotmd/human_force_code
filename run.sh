5deda8983df2754f0a396689313bd49024be063a
talbot <talbotmd@stanford.edu>
2017-07-12 14:49:09 -0700

    ran trials and changed run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
-cd optoforcesensors/sensor2/build/
-x-terminal-emulator -e "bash -c 'echo \"Plug in second force sensor before running this\";
+cd optoforcesensors/sensor1/build/
+# x-terminal-emulator -e "bash -c 'echo \"Run this one first\" && sudo ./optoforce_driver'"
+# cd ../../..
+x-terminal-emulator -e "bash -c 'echo \"Run this second, with the round force sensor\";
 while true; do
 	sudo ./optoforce_driver
 done
 '"
 cd ../../..
 
-cd optoforcesensors/sensor1/build/
-# x-terminal-emulator -e "bash -c 'echo \"Run this one first\" && sudo ./optoforce_driver'"
-# cd ../../..
-x-terminal-emulator -e "bash -c 'echo \"Run this one first, with the round force sensor\";
+cd optoforcesensors/sensor2/build/
+x-terminal-emulator -e "bash -c 'echo \"Run this first, with the square sensor\";
 while true; do
 	sudo ./optoforce_driver
 done
@@ -18,4 +18,6 @@ cd ../../..
 
 read -p "Once both sensors are running, press any key to continue..." yn
 
-./optitrack/bin/demo_project
+cd optitrack/bin
+./demo_project
+cd ../..
